5bd3d020d5e698db5157ed855d096c5780181324
Andrehlb <andrehlb@me.com>
2024-03-30 20:47:28 -0300

    feat: Add `ChampionsTraducional.java` file.

---
+++ src/main/java/me/dio/sdw4Andrehlb/domain.model/ChampionsTradicional.java
@@ -0,0 +1,9 @@
+package me.dio.sdw4Andrehlb.domain.model;
+
+public class ChampionsTradicional {
+    private String id;
+    private String name;
+    private String role;
+    private String lore;
+    private String imageUrl;
+}
